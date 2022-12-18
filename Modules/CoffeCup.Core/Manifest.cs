d1662cff1b7dd46bceb4e6c8a8138d54a879620c
Aro <arohunanian@gmail.com>
2022-12-19 01:36:25 +0400

    first commit

---
+++ Modules/CoffeCup.Core/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "CoffeCup.Core",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "CoffeCup.Core",
+    Category = "Content Management"
+)]
